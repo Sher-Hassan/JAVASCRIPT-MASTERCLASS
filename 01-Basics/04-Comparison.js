dd7ad2d013d24604661f221333240b115ea0db98
Sher Hassan <sherhassan739@gmail.com>
2024-12-03 18:10:20 +0500

    Comparison done

---
+++ 01-Basics/04-Comparison.js
@@ -0,0 +1,13 @@
+console.log(2 > 1); // true
+console.log(2 < 1); // false
+console.log(2 >= 1); // true
+console.log(2 <= 1); // false
+console.log(2 == 1); // false
+console.log(2 != 1); // true
+
+console.log("2" > 1); // true
+console.log("2" < 1); // false
+
+console.log(null == undefined); // true
+console.log(null > 0); // false
+console.log(null >= 0); // true
