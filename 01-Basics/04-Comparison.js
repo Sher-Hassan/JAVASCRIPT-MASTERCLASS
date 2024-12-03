a7c19e26d17f79e639214b152459be1f91c77cd2
Sher Hassan <sherhassan739@gmail.com>
2024-12-03 18:23:13 +0500

    Datatypes Summary

--- 01-Basics/04-Comparison.js
+++ 01-Basics/04-Comparison.js
@@ -11,3 +11,9 @@ console.log("2" < 1); // false
 console.log(null == undefined); // true
 console.log(null > 0); // false
 console.log(null >= 0); // true
+
+const myFunction = function() {
+    console.log("Hello");
+}
+
+console.log(typeof myFunction); // [Function: myFunction]
