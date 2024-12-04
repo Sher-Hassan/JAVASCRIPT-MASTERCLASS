a13c4669b411870f020bf92027f956dfd3ab1f86
Sher Hassan <sherhassan739@gmail.com>
2024-12-05 00:48:19 +0500

    Json intro of APIs talk Objects Destructuring

--- 02-Basics/04-Objects-2.js
+++ 02-Basics/04-Objects-2.js
@@ -1,7 +1,7 @@
 // const tinderUser = new Object();
 const tinderUser = {};
 
-console.log(tinderUser); // {}
+// console.log(tinderUser); // {}
 
 // we can add properties to an object
 
@@ -21,9 +21,9 @@ const regularUser = {
     }
 }
 
-console.log(regularUser.fullname.userfullname.firstName); // sher
+// console.log(regularUser.fullname.userfullname.firstName); // sher
 
-console.log(regularUser.fullname?.userfullname.firstName); // ? is used to avoid error if the property is not present
+// console.log(regularUser.fullname?.userfullname.firstName); // ? is used to avoid error if the property is not present
 
 //Combining objects
 
@@ -74,21 +74,67 @@ const users = {
     }
 }
 
-console.log(users[1].name); // sher
+// console.log(users[1].name); // sher
 
-console.log(tinderUser);
+// console.log(tinderUser);
 
-console.log(Object.keys(tinderUser)); // [ 'name', 'age', 'isLoggedin' ] and datatype is array which is very useful and important to use keys 
+// console.log(Object.keys(tinderUser)); // [ 'name', 'age', 'isLoggedin' ] and datatype is array which is very useful and important to use keys 
 
 
-console.log(Object.values(tinderUser)); // [ 'sher', 18, false ] 
+// console.log(Object.values(tinderUser)); // [ 'sher', 18, false ] 
 
-console.log(Object.entries(tinderUser)); // [ [ 'name', 'sher' ], [ 'age', 18 ], [ 'isLoggedin', false ] ] it will return an array of arrays
+// console.log(Object.entries(tinderUser)); // [ [ 'name', 'sher' ], [ 'age', 18 ], [ 'isLoggedin', false ] ] it will return an array of arrays
 
 // When we are looping through and we are looking for a property but it doesnot exists so it can crash the program so we can use the following method to avoid the crash
 
-console.log(tinderUser.hasOwnProperty("name")); // true
+// console.log(tinderUser.hasOwnProperty("name")); // true
 
-console.log(tinderUser.hasOwnProperty("email")); // false
+// console.log(tinderUser.hasOwnProperty("email")); // false
 
+// Object destructuring
+
+const course = {
+    coursename: "JS",
+    coursefee: "1000",
+    courseinstructor: "sher"
+}
+
+
+// course.coursename // JS
+//but to write clean code we can use this method
+// Extracting the properties from the object is used in API's and when we are getting data from the database in react 
+
+// const {courseinstructor} = course; // it will extract the courseinstructor from the course object
+// console.log(courseinstructor); // sher
+
+//now we can give our own name to the extracted property
+
+const {courseinstructor: instructor} = course; // it will extract the courseinstructor from the course object and will give it the name instructor
+
+console.log(instructor); // sher
+
+//React example of destructring props
+
+// const navbar = (props.company) => {} // this is the old way of destructuring
+// navbar(company = "sher")
+// const navbar = ({company}) => {} // this is the new way of destructuring
+
+// API's
+
+// How json looks when data comes from API
+
+// {
+//     "name": "sher",
+//     "age": 18,
+//     "email": "SherHassan@gmail.com"
+// }
+
+// API example: https://api.github.com/users/hiteshchoudhary
+
+//Convert APIs to objects then extract values
+// API's can also be in form of arrays
+
+//API example: https://randomuser.me/
+
+// To get the right struture of API we can use json formatter and select tree structure to see the structure of the API
 
